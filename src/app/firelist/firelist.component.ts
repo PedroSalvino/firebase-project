474a15c5242fb591060944744cd07827cd0fb2b5
PedroSalvino <pedro_luiz7199@outlook.com>
2021-11-05 11:30:29 -0300

    2 files modified

--- src/app/firelist/firelist.component.ts
+++ src/app/firelist/firelist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+
 import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
