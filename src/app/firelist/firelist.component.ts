35714da325bb50c08ed5e64ec0445078d64119af
PedroSalvino <pedro_luiz7199@outlook.com>
2021-11-05 05:37:33 -0300

    1 file modified

--- src/app/firelist/firelist.component.ts
+++ src/app/firelist/firelist.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFireList } from '@angular/fire/database/database';
+import {
+  AngularFireDatabase,
+  AngularFireList,
+} from '@angular/fire/database/database';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-firelist',
@@ -11,13 +15,29 @@ export class FirelistComponent implements OnInit {
   text: string;
 
   listRef: AngularFireList<any>;
-  list: Observable<any[]>;
+  list: Observable<any>;
 
-  constructor() {}
+  constructor(private db: AngularFireDatabase) {
+    this.listRef = db.list('list');
+    this.list = this.listRef
+      .snapshotChanges()
+      .pipe(
+        map((changes) =>
+          changes.map((c) => ({ key: c.payload.key, ...c.payload.val() }))
+        )
+      );
+  }
 
   ngOnInit() {}
 
-  addItem(){
-    
+  addItem() {
+    this.listRef.push({
+      text: this.text,
+    });
+    this.text = '';
+  }
+
+  deleteItem(key: string) {
+    this.listRef.remove(key);
   }
 }
